0059be899f6ffd1a21b20bf64b33866d2bfe2a6a
kudawoo2002 <kudawoo2002@gmail.com>
2024-02-13 16:48:16 +0000

    Adding form and packaging list component

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Charger", quantity: 1, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -15,16 +21,42 @@ function Logo() {
 
 function Form() {
   return (
-    <div className="add-form">
+    <Form className="add-form">
       <h3>What you need for your trip</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            num
+          </option>
+        ))}
+      </select>
+      <input type="text" name="itemName" placeholder="Item...." />
+      <button>Add</button>
+    </Form>
   );
 }
 
 function PackagingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item itemobj={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
-
+function Item({ itemobj }) {
+  return (
+    <li>
+      <span style={itemobj.packed ? { textDecoration: "line-through" } : {}}>
+        {itemobj.quantity} {itemobj.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
 function Stats() {
   return (
     <footer className="stats">
