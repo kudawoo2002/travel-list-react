98cd7f5b97b361ee3e8d0b148e99788d29a04e02
kudawoo2002 <kudawoo2002@gmail.com>
2024-02-13 15:43:45 +0000

    initial Setup

---
+++ travel-list/src/App.js
@@ -0,0 +1,34 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackagingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What you need for your trip</h3>
+    </div>
+  );
+}
+
+function PackagingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>You have X item on your list,and you already packed x (x%)</em>
+    </footer>
+  );
+}
